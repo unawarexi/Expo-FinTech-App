50a989dbaeeceb23e1f7ff3bf78c2038a87f17e4
unawarexi <unawarexi@gmail.com>
2024-05-03 22:40:42 +0100

    OnBoarding Screen  &  VideoBg Using Expo-assest/av

--- fintech/app/index.tsx
+++ fintech/app/index.tsx
@@ -1,44 +1,59 @@
-import Colors from '@/constants/Colors';
-import { defaultStyles } from '@/constants/Styles';
-import { useAssets } from 'expo-asset';
-import { ResizeMode, Video } from 'expo-av';
-import { Link } from 'expo-router';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import { useAssets } from "expo-asset";
+import { ResizeMode, Video } from "expo-av";
+import { Link } from "expo-router";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const Page = () => {
   // const [assets] = useAssets([require('https://videos.pexels.com/video-files/7579572/7579572-hd_720_1366_25fps.mp4')]);
 
   return (
     <View style={styles.container}>
-     
-        <Video
-          resizeMode={ResizeMode.COVER}
-          isMuted
-          isLooping
-          shouldPlay
-          source={{ uri: "https://videos.pexels.com/video-files/6699610/6699610-sd_540_960_25fps.mp4" }}
-          style={styles.video}
-        />
-     
+      {/* {===========================  for background video =============================} */}
+      <Video
+        resizeMode={ResizeMode.COVER}
+        isMuted
+        isLooping
+        shouldPlay
+        source={{
+          uri: "https://videos.pexels.com/video-files/6699610/6699610-sd_540_960_25fps.mp4",
+        }}
+        style={styles.video}
+      />
+
       <View style={{ marginTop: 80, padding: 20 }}>
         <Text style={styles.header}>Ready to change the way you money?</Text>
       </View>
 
+      {/* {===========================  for Login button =============================} */}
       <View style={styles.buttons}>
         <Link
-          href={'/login'}
-          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: Colors.dark }]}
-          asChild>
+          href={"/Login"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: Colors.dark },
+          ]}
+          asChild
+        >
           <TouchableOpacity>
-            <Text style={{ color: 'white', fontSize: 22, fontWeight: '500' }}>Log in</Text>
+            <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+              Log in
+            </Text>
           </TouchableOpacity>
         </Link>
+
+        {/* {===========================  for signUp button =============================} */}
         <Link
-          href={'/signup'}
-          style={[defaultStyles.pillButton, { flex: 1, backgroundColor: '#fff' }]}
-          asChild>
+          href={"/SignUp"}
+          style={[
+            defaultStyles.pillButton,
+            { flex: 1, backgroundColor: "#fff" },
+          ]}
+          asChild
+        >
           <TouchableOpacity>
-            <Text style={{ fontSize: 22, fontWeight: '500' }}>Sign up</Text>
+            <Text style={{ fontSize: 22, fontWeight: "500" }}>Sign up</Text>
           </TouchableOpacity>
         </Link>
       </View>
@@ -49,22 +64,22 @@ const Page = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
   },
   video: {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
+    width: "100%",
+    height: "100%",
+    position: "absolute",
   },
   header: {
     fontSize: 36,
-    fontWeight: '900',
-    textTransform: 'uppercase',
-    color: 'white',
+    fontWeight: "900",
+    textTransform: "uppercase",
+    color: "white",
   },
   buttons: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     gap: 20,
     marginBottom: 60,
     paddingHorizontal: 20,
