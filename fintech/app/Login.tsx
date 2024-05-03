de3d611ff1555aaf41df77e48f1b4225dd44e672
unawarexi <unawarexi@gmail.com>
2024-05-03 22:40:56 +0100

    Login Page

--- fintech/app/Login.tsx
+++ fintech/app/Login.tsx
@@ -1,10 +1,105 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
+import Colors from "@/constants/Colors";
+import { defaultStyles } from "@/constants/Styles";
+import React, { useState } from "react";
+import { Link } from "expo-router";
 
 export default function Login() {
+  const [countryCode, setCountryCode] = useState("+49");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 80 : 0;
+  const onSignUp = async () => {
+    // const fullPhoneNumber = `${countryCode}${phoneNumber}`;
+    // try {
+    //   await signUp!.create({
+    //     phoneNumber: fullPhoneNumber,
+    //   });
+    //   signUp!.preparePhoneNumberVerification();
+    //   router.push({ pathname: '/verify/[phone]', params: { phone: fullPhoneNumber } });
+    // } catch (error) {
+    //   console.error('Error signing up:', error);
+    // }
+  };
+
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior="padding"
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <View style={defaultStyles.container}>
+        <Text style={defaultStyles.header}>Welcome back !</Text>
+        <Text style={defaultStyles.descriptionText}>
+          Enter your phone number. We will send you a confirmation code there
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Country code"
+            placeholderTextColor={Colors.gray}
+            value={countryCode}
+          />
+          <TextInput
+            style={[styles.input, { flex: 1 }]}
+            placeholder="Mobile number"
+            placeholderTextColor={Colors.gray}
+            keyboardType="numeric"
+            value={phoneNumber}
+            onChangeText={setPhoneNumber}
+          />
+        </View>
+
+        <Link href={"/SignUp"} replace asChild>
+          <TouchableOpacity>
+            <Text style={defaultStyles.textLink}>
+              Don't have an account? Sign Up
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <View style={{ flex: 1 }} />
+
+        {/* {===========================  for signUp button =============================} */}
+        <TouchableOpacity
+          style={[
+            defaultStyles.pillButton,
+            phoneNumber !== "" ? styles.enabled : styles.disabled,
+            { marginBottom: 20 },
+          ]}
+          onPress={onSignUp}
+        >
+          <Text style={defaultStyles.buttonText}> Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
+
+/* {=========================== UNIQUE PAGE STYLESHEET =============================} */
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 40,
+    flexDirection: "row",
+  },
+  input: {
+    backgroundColor: Colors.lightGray,
+    padding: 20,
+    borderRadius: 16,
+    fontSize: 20,
+    marginRight: 10,
+  },
+  enabled: {
+    backgroundColor: Colors.primary,
+  },
+  disabled: {
+    backgroundColor: Colors.primaryMuted,
+  },
+});
